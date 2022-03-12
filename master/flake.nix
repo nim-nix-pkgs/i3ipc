a6642769ad3c0ef054e14ec386dd9409693cbb5c
hugosenari <hugosenari@gmail.com>
2022-03-12 01:53:30 -0300

    chore: re index i3ipc

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''i3 IPC client library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-i3ipc-master.flake = false;
+  inputs.src-i3ipc-master.owner = "FedericoCeratto";
+  inputs.src-i3ipc-master.ref   = "refs/heads/master";
+  inputs.src-i3ipc-master.repo  = "nim-i3ipc";
+  inputs.src-i3ipc-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-i3ipc-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-i3ipc-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
